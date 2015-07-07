252c412126f76a8dcd0bdfae537e3e01826d83d9
sobird <crossyou2009@gmail.com>
2015-07-07 17:01:43 +0800

    v0.0.1

---
+++ index.js
@@ -0,0 +1,15 @@
+/*
+ * fis-optimizer-php-compactor
+ *
+ * Copyright (c) 2015 Baidu EXP Team
+ * Licensed under the MIT license.
+ * https://github.com/exp-team/fis-optimizer-tpl-compress/blob/master/LICENSE
+ */
+
+'use strict';
+
+module.exports = function(content, file, conf){
+    //TODO
+    
+    return content;
+};
