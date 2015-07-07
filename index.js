b8be25554ac6daa87fac436b0d0c6eb850abe4a2
sobird <crossyou2009@gmail.com>
2015-07-07 18:13:49 +0800

    base dev

--- index.js
+++ index.js
@@ -1,5 +1,5 @@
 /*
- * fis-optimizer-php-compactor
+ * fis-optimizer-tpl-compress
  *
  * Copyright (c) 2015 Baidu EXP Team
  * Licensed under the MIT license.
@@ -8,8 +8,11 @@
 
 'use strict';
 
+var compress = require('./lib/compress.js');
+
 module.exports = function(content, file, conf){
-    //TODO
-    
-    return content;
+    conf['leftDelimiter'] = conf['leftDelimiter'] || fis.config.get('settings.smarty.left_delimiter') || '{%';
+    conf['rightDelimiter'] = conf['rightDelimiter'] || fis.config.get('settings.smarty.right_delimiter') || '%}';
+    conf['level'] = conf['conf'] || 'strip';
+    return compress(content, conf);
 };
